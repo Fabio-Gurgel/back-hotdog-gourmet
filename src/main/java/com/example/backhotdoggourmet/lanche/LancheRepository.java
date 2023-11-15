575e72b33290374d781396a85789e573b9c425fe
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2023-11-15 11:49:38 -0300

    Criando service e repository de lanche

---
+++ src/main/java/com/example/backhotdoggourmet/lanche/LancheRepository.java
@@ -0,0 +1,7 @@
+package com.example.backhotdoggourmet.lanche;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LancheRepository extends JpaRepository<Lanche, Long>{
+
+}
--- src/main/java/com/example/backhotdoggourmet/lanche/LancheService.java
+++ src/main/java/com/example/backhotdoggourmet/lanche/LancheService.java
@@ -1,5 +1,49 @@
 package com.example.backhotdoggourmet.lanche;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backhotdoggourmet.exceptions.ResourceNotFoundException;
+
+@Service
 public class LancheService {
     
+    @Autowired
+    LancheRepository lancheRepository;
+
+    public List<Lanche> getAllLanches() {
+        return lancheRepository.findAll();
+    }
+
+    public Lanche getLancheById(Long id) {
+        Optional<Lanche> lancheEncontrado = lancheRepository.findById(id);
+
+        if(lancheEncontrado.isEmpty()){
+            throw new ResourceNotFoundException("Não foi possível encontrar o lanche com id " + id);
+        }
+
+        return lancheEncontrado.get();
+    }
+
+    public Lanche createLanche(Lanche lanche) {
+        return lancheRepository.save(lanche);
+    }
+
+    public Lanche updateLanche(Long id, Lanche lanche) {
+        Lanche lancheExistente = getLancheById(id);
+
+        lancheExistente.setNome(lanche.getNome());
+        lancheExistente.setPreco(lanche.getPreco());
+        lancheExistente.setIngredientes(lanche.getIngredientes());
+
+        return lancheRepository.save(lancheExistente);
+    }
+
+    public void deleteLanche(Long id) {
+        getLancheById(id);
+        lancheRepository.deleteById(id);
+    }
 }
