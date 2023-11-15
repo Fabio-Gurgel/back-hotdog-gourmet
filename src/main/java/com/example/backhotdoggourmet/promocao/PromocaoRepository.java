068e59129bf72ee3b4db54c5820760a85962c5a4
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2023-11-15 11:14:25 -0300

    Criando service e repository de promocao

---
+++ src/main/java/com/example/backhotdoggourmet/promocao/PromocaoRepository.java
@@ -0,0 +1,7 @@
+package com.example.backhotdoggourmet.promocao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+    
+}
--- src/main/java/com/example/backhotdoggourmet/promocao/promocaoService.java
+++ src/main/java/com/example/backhotdoggourmet/promocao/promocaoService.java
@@ -1,5 +1,49 @@
 package com.example.backhotdoggourmet.promocao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.backhotdoggourmet.exceptions.ResourceNotFoundException;
+
 public class PromocaoService {
+
+    @Autowired
+    PromocaoRepository promocaoRepository;
+
+    public List<Promocao> getAllPromocoes() {
+        return promocaoRepository.findAll();
+    }
+
+    public Promocao getPromocaoById(Long id) {
+        Optional<Promocao> promocaoEncontrada = promocaoRepository.findById(id);
+
+        if(promocaoEncontrada.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi possível encontrar a promoção com id " + id);
+        }
+
+        return promocaoEncontrada.get();
+    }
+
+    public Promocao createPromocao(Promocao promocao) {
+        return promocaoRepository.save(promocao);
+    }
+
+    public Promocao updatePromocao(Long id, Promocao promocao) {
+        Promocao promocaoExistente = getPromocaoById(id);
+
+        promocaoExistente.setNome(promocao.getNome());
+        promocaoExistente.setDescricao(promocao.getDescricao());
+        promocaoExistente.setPreco(promocao.getPreco());
+        promocaoExistente.setLanche(promocao.getLanche());
+
+        return promocaoRepository.save(promocaoExistente);
+    }
+
+    public void deletePromocao(Long id) {
+        getPromocaoById(id);
+        promocaoRepository.deleteById(id);
+    }
     
 }
